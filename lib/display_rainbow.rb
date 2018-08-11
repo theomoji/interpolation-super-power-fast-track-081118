fae1cf95d3772961437ac327f7327f693c9aba3e
Shuo Wang <gken90@gmail.com>
2018-08-11 17:38:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -12,22 +12,22 @@
 #
 # You must read from the colors argument and you can hardcode the order, there is no need to use #each or any loop. For example,
 #given letters = ["b","a","c"] to print them in alphabetical order without iteration you could:
-def display_rainbow(color_array)
-  puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B: #{color_array[4]}, I: #{color_array[5]}, V: #{color_array[6]}"
-end
-
-
-
-
-# def display_rainbow(colors)
-#   colored = []
-#   colors_string = ""
-#   index = 0
-#   while index < colors.length
-#     color = colors[index]
-#     first_letter = color[0].capitalize
-#     color_format = "#{first_letter}: #{color}"
-#     colored << color_format
-#     index += 1
-#   end
+# def display_rainbow(color_array)
+#   puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B: #{color_array[4]}, I: #{color_array[5]}, V: #{color_array[6]}"
 # end
+
+
+
+
+def display_rainbow(colors)
+  colored = []
+  colors_string = ""
+  index = 0
+  while index < colors.length
+    color = colors[index]
+    first_letter = color[0].capitalize
+    color_format = "#{first_letter}: #{color}"
+    colored << color_format
+    index += 1
+  end
+end
